a824039539a439bd93ff9a589c544899638aa4f0
Guoxh <AGuoxh@163.com>
2020-01-08 11:01:53 +0800

    样式完结提交, 下一版将添加新的页面和功能

--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -1,16 +1,65 @@
 <template>
   <div class="box">
-    首页
+    <div class="home-top">
+      <home-card title="小区总户数" count="450" :fontColor="{color: '#00A8FF'}" :logColor="{backgroundColor: '#00A8FF'}">
+        <div class="home-date" slot="date"> 当前月份:2019-10</div>
+        <template slot="log"><i class="iconfont icon-home-top1"></i></template>
+      </home-card>
+      <home-card title="在住户数" count="3080" :fontColor="{color: '#FD8C40'}" :logColor="{backgroundColor: '#FD8C40'}">
+        <template slot="log"><i class="iconfont icon-home-top2"></i></template>
+      </home-card>
+      <home-card title="月缴费总额" count="1000" :fontColor="{color: '#00A8FF'}" :logColor="{backgroundColor: '#00A8FF'}">
+        <div class="home-date" slot="date"> 当前月份:2019-10</div>
+        <div class="home-money" slot="money">小程序支付:<span class="home-money-font"> ¥{{1000}}</span></div>
+        <template slot="log"><i class="iconfont icon-home-top3"></i></template>
+      </home-card>
+      <home-card title="月缴费总额" count="23800" :fontColor="{color: '#50CC43'}" :logColor="{backgroundColor: '#50CC43'}">
+        <div class="home-date" slot="date"> 当前月份:2019-10</div>
+        <template slot="log"><i class="iconfont icon-home-top4"></i></template>
+      </home-card>
+      <home-card title="已缴费户数" count="2980" :fontColor="{color: '#00A8FF'}" :logColor="{backgroundColor: '#00A8FF'}">
+        <div class="home-date" slot="date"> 当前月份:2019-10</div>
+        <template slot="log"><i class="iconfont icon-home-top5"></i></template>
+      </home-card>
+      <home-card title="未缴费户数" count="100" :fontColor="{color: '#50CC43'}" :logColor="{backgroundColor: '#50CC43'}">
+        <div class="home-date" slot="date"> 当前月份:2019-10</div>
+        <template slot="log"><i class="iconfont icon-home-top6"></i></template>
+      </home-card>
+    </div>
+
+    <div class="home-chart">
+      <div class="home-chartLine">缴费统计</div>
+      <div class="home-chartNotice">最新公告</div>
+    </div>
   </div>
+
 </template>
 
 <script>
 
+  import Homecard from '@/components/Homecard'
+
   export default {
-    name: "Home"
+    name: "Home",
+    components: {
+      'home-card': Homecard
+    },
+    data() {
+      return {
+      }
+    }
   }
 </script>
 
 <style scoped>
+  .box { width: 100%; }
+  .home-top { display: flex; flex-wrap: wrap; justify-content: space-between; width: 100%; }
+  .home-date { color: #999; }
+  .home-money { color: #999; }
+  .home-money-font { color: #00A8FF; }
+
+  .home-chart { width: 100%; height: 555px; display: flex; justify-content: space-between; }
+  .home-chartLine { width: 66%; background-color: #fff; }
+  .home-chartNotice { width: 32%; background-color: #FFF; }
 
 </style>
--- src/components/Homecard/index.vue
+++ src/components/Homecard/index.vue
@@ -1,17 +1,47 @@
 <template>
-    <div>
-        <el-card>
-            
-        </el-card>
-    </div>
+      <div class="homecard">
+        <div class="homecard-content">
+          <div>{{title}} <span class="homecard-content-span" :style="fontColor">  {{count}}</span></div>
+          <slot name="date"/>
+          <slot name="money"/>
+        </div>
+
+        <div class="homecard-log" :style="logColor">
+          <slot name="log"/>
+        </div>
+      </div>
 </template>
 
 <script>
   export default {
-    name: 'Homecard'
+    name: 'Homecard',
+    props: {
+      title: {
+        type: String,
+        required: true
+      },
+      count: {
+        type: String,
+        required: true
+      },
+      fontColor: {
+        type: Object,
+        required: true,
+      },
+      logColor: {
+        type: Object,
+        required: true
+      }
+    },
   }
 </script>
 
 <style scoped>
-.el-card { width: 31%;}
+  .homecard { padding: 30px; width: 27%; margin-bottom: 20px; background-color: #fff; border-radius: 4px; display: flex; justify-content: space-between; box-shadow: 0 1px 1px rgba(0,0,0,0.15)}
+  .homecard-content { height: 102px; display: flex; flex-direction: column; justify-content: space-around; }
+  .homecard-content-span { font-size: 28px; }
+  .homecard-log { height: 100px; width: 100px; border-radius: 50%; display: flex; justify-content: center;}
+  .homecard-log i { color: #fff; font-size: 80px; line-height: 100px;}
+  .iconfont { margin: 0;}
+
 </style>
