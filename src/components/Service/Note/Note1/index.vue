f618ea2d315024ad8caa168ac8ca768a2db2bf22
Guoxh <AGuoxh@163.com>
2020-01-10 17:56:54 +0800

    版本二 基础架构002

--- src/components/Service/Note/Note1/index.vue
+++ src/components/Service/Note/Note1/index.vue
@@ -1,9 +1,12 @@
 <!-- 小区服务下的  短信发送页面-->
 <template>
-  <div>小区服务>> 短信发送</div>
+  <main-card title="短信发送">
+    <div slot="content"> 这是内容展示部分 </div>
+  </main-card>
 </template>
 
 <script>
+
   export default {
     name: "Note1"
   }
--- src/components/Service/Note/Note2/index.vue
+++ src/components/Service/Note/Note2/index.vue
@@ -1,9 +1,12 @@
 <!-- 小区服务下的  单条发送-->
 <template>
-  <div>小区服务>> 单条发送</div>
+  <main-card title="单条发送">
+    <div slot="content"> 这是内容展示部分 </div>
+  </main-card>
 </template>
 
 <script>
+
   export default {
     name: "Note2"
   }
